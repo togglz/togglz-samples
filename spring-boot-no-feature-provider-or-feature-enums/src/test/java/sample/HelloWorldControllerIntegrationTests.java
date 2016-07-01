ddc23142877f52b95c32e65009859e38d425892e
Marcel Overdijk <marcel@overdijk.me>
2016-07-01 10:21:54 +0200

    Added new sample

---
+++ spring-boot-no-feature-provider-or-feature-enums/src/test/java/sample/HelloWorldControllerIntegrationTests.java
@@ -0,0 +1,43 @@
+package sample;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+public class HelloWorldControllerIntegrationTests {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUpMockMvc() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    public void testHelloWorld() throws Exception {
+        mockMvc.perform(get(""))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Greetings from Spring Boot!"));
+    }
+}
